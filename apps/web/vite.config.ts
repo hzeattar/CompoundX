12a44236372e424cfb2bb93515c414cbaf6b6d58
hzeattar <hzeattar@users.noreply.github.com>
2026-04-08 22:45:46 +0200

    feat: deploy web demo to github pages

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/CompoundX/" : "/",
   plugins: [vue()],
   server: {
     host: "0.0.0.0",
     port: 5173
   }
-});
+}));
